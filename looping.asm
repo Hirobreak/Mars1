fa115a59430fd87444d2d98ad7b06b78141d638c
Pedro Iñiguez <mistery7@hotmail.es>
2014-06-21 18:48:45 -0500

    pilas primera parte de los nums
    locos?

--- looping.asm
+++ looping.asm
@@ -1,47 +1,62 @@
 
 	.data
 fout: .asciiz "pilas2.txt" 
-buffer: .resb 10
-
+buffer: .resb 33
 
 	.text
 	# open file
-	li $v0, 13 
-	la $a0, fout
+	li $v0, 13 #instruccion abrir archivo
+	la $a0, fout #parametro nombre
 	li $a1, 1
 	li $a2, 0
-	syscall
-	move $s6, $v0
+	syscall #llamo a la instrucción
+	move $s6, $v0 #guardo el registro de información del archivo en s6
 	
-	li $a1, 200
-	li $v0, 42
-	syscall
-	li $v0, 1
-	syscall
+	li $a1, 200 #establesco 200 como random maximo
+	li $v0, 42 #instrucción para random
+	syscall #ejecuto
+	li $v0, 1 #intruccion print de $a0 donde esta el random
+	syscall #ejecuto
 
-	la $t3, buffer
-
+	la $s3, buffer #cargo buffer
+	
+	addi $sp, $sp, -12 #desplazo 3*4 bytes la pila
+	
 loop1:	addi $t0, $zero, 10
 	div  $a0, $t0       # $a0/10
 	mflo $a1           # $a1 = quotient
 	mfhi $t0           # $t0 = remainder
 	addi $a0, $t0, 0x30    # convert to ASCII
-	#sll $t0, $t0, 8
-	#or $a3, $a1, $t0
-	add $t2, $a0, $zero
-	sb $t2, buffer
+	add $t2, $a0, $zero	#cargo en t2 el residuo en ascii
+	sw $t2, ($sp)	#guardo t2 en la pila
 	
-	add $t4, $a1, $zero
-	
+	addi $sp, $sp, 4
+	add $a0, $a1, $zero
+	bne $a0, $zero, loop1
+	#Grabar primer numero
+	addi $sp, $sp, -4 #me desplazo 4 bytes hacia abajo en la pila para el primer digito
+	#write file
+	li $v0, 15 #instruccion write
+	move $a0, $s6 #cargo la descripcion del archivo que tenia guardada en s6
+	move $a1, $sp #pongo  el valor del digito en ascii como parametro
+	li $a2, 1 
+	syscall #exporto el num al archivo
+	#Grabar segundo numero
+	addi $sp, $sp, -4
 	#write file
 	li $v0, 15
 	move $a0, $s6
-	move $a1, $t3
-	li $a2, 3
+	move $a1, $sp
+	li $a2, 1
 	syscall
-	
-	add $a0, $t4, $zero
-	bne $a0, $zero, loop1
+	#Grabar tercer numero
+	addi $sp, $sp, -4
+	#write file
+	li $v0, 15
+	move $a0, $s6
+	move $a1, $sp
+	li $a2, 1
+	syscall
 	
 	#close file
 	li $v0, 16
