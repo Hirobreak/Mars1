dd2cb9cb8a72246fd11e2d9cb6c25ab0847bded2
Pedro Iñiguez <mistery7@hotmail.es>
2014-06-28 19:20:50 -0500

    arreglo de 10 items contiene primer set

--- reading.asm
+++ reading.asm
@@ -1,6 +1,7 @@
 
 	.data
 fout: .asciiz "pilas2.txt" 
+arreglo: .word  1 : 10
 buffer: .resb 33
 	.text
 	
@@ -11,12 +12,14 @@ buffer: .resb 33
 	li $a2, 0
 	syscall #llamo a la instrucción
 	move $s6, $v0 #guardo el registro de información del archivo en s6
+	li $s0, 0 #indice del arreglo
 	la $s3, buffer #cargo buffer
 	addi $sp, $sp, -16 #bajo en la pila
 	addi $t2, $zero, 0 #cargo para comparar el Null
 	addi $t5, $zero, 32 #cargo para comparar el espacio
 	addi $t3, $zero, 44 #cargo para comparar la coma
 	addi $t9, $zero, 47 #cargo para comparar el slash
+	move $t0, $zero #contador de iteraciones lectura de numeros
 loop3:
 	add $t7, $zero, $zero #contador de digitos del numero
 	#read file
@@ -52,12 +55,36 @@ loop2:	lw $t1, ($sp) #cargo el primer valor de la pila
 	addi $t7, $t7, -1 #revierto el contador 
 	bne $t7, $zero, loop2 #verifico si es cero el contador
 	
+	addi $s0, $s0, 4 #avanzo el indice del arreglo en 4 bytes
+	sw $t8, arreglo($s0) #accedo a la posicion del arreglo segun el valor de $s0, como en c arreglo[s0]
+	addi $t0, $t0, 1 #aumento el contador de numeros
+	lw $t8, arreglo($s0) #IMPRIMO CADA ELEMENTO DEL ARREGLO PARA SABER QUE ESTAN BIEN GUARDADOS
 	add $a0, $zero, $t8 #preparo para imprimir
 	li $v0, 1 #intruccion print 
 	syscall #ejecuto
 
 	beq $zero, $zero, loop3 #regreso al loop de sacar numeros
 salSls:
+	#BubbleSort
+	li $s0, 0 #inicializo el indice del arreglo
+	addi $s0, $s0, 4
+loop4:	lw $t1, arreglo($s0)
+	addi $s0, $s0, 4
+	lw $t2, arreglo($s0)
+	slt $s1, $t1, $t2
+	bne $s1, $zero, ordered
+	sw $t1, arreglo($s0)
+	addi $s0, $s0, -4
+	sw $t2, arreglo($s0)
+	addi $s0, $s0, 4
+ordered:
+	lw $t8, arreglo($s0)
+	add $a0, $zero, $t8 #preparo para imprimir
+	li $v0, 1 #intruccion print 
+	syscall #ejecuto
+	subi $t0, $t0, 1
+	bne $t0, $zero, loop4
+	
 	#close file
 	li $v0, 16
 	move $a0, $s6
