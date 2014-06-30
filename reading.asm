bbacfe9758a9b62de6894b2c619281a170fb5378
unknown <gaumala@espol.edu.ec>
2014-06-29 19:12:39 -0500

    Bubble Sort y Insert Sort listos modificando el registro t7 se pude elegir el algoritmo, 0 para bubble, 1 para insert.

--- reading.asm
+++ reading.asm
@@ -2,10 +2,12 @@
 	.data
 fout: .asciiz "aleatorios.txt" 
 foutbub: .asciiz "bur_aleatorios.txt" 
+foutins: .asciiz "ins_aleatorios.txt" 
 arreglo: .word  1 : 401
+op: .word 0
 buffer: .resb 33
 	.text
-		
+	addi $t7, $zero, 0	#0 para bubble sort, 1 para insert sort
 	li $s3, 10
 	# open file
 	li $v0, 13 #instruccion abrir archivo
@@ -17,7 +19,9 @@ buffer: .resb 33
 	# open file
 	li $v0, 13 #instruccion abrir archivo
 	la $a0, foutbub #parametro nombre
-	li $a1, 1
+	beq $t7, $zero, bubble1
+	la $a0, foutins 
+bubble1: li $a1, 1
 	li $a2, 0
 	syscall #llamo a la instrucción
 	move $s5, $v0 #guardo el registro de información del archivo en s6
@@ -75,7 +79,10 @@ loop2:	lw $t1, ($sp) #cargo el primer valor de la pila
 
 	beq $zero, $zero, loop3 #regreso al loop de sacar numeros
 salSls:
-	#BubbleSort
+	
+	beq $t7, $zero, bubble_start
+	j insert_sort	
+bubble_start:	#BubbleSort
 	li $v0, 30 #preparo parametro funcion obtener tiempo
 	syscall #llamo a la funcion obtener tiempo
 	move $s7, $a0 #guardo el tiempo en s7
@@ -101,6 +108,35 @@ ordered:
 	bne $t0, $zero, loop5 #salto de regreso si aun hay iteraciones restantes
 	
 	li $s0, 0 #inicializo el indice del arreglo
+	#Fin de BubbleSort
+	bne $t7, $zero, insert_sort
+	j imp
+	#INSERTION SORT
+insert_sort:
+	add $s2, $zero, $t0	# $s2 = numero de elementos a organizar
+	addi $t0, $t0, 1	# $t0 = longitud del arreglo + 1 (Porque el indice comienza dezde 1)
+	li $s1, 2		# $s1 = indice del primer elemento sin ordenar
+lazo_for:
+	beq 	$t0, $s1, imp	# Si el indice incrementa hasta la longitud del arreglo se termina el lazo
+	subi 	$s4, $s1, 1		# $s4 = indice de prueba
+	sll	$s0, $s1, 2		# $s0 = direccion a la que apunta el indice sin ordenar. Desplazar a la izq 2 veces para byte offset
+	lw	$t1, arreglo($s0)	# $t1 = elemento a insertar
+lazo_while:
+	beq 	$s4, $zero, fin_del_wh	# Si el indice de prueba es menor a cero se acaba el while
+	sll	$s0, $s4, 2		# $s0 = direccion a la que apunta el indice de prueba. Desplazar a la izq 2 veces para byte offset
+	lw	$t2, arreglo($s0)	# $t2 = elemento de prueba
+	ble	$t2, $t1, fin_del_wh	# Si el elemento de prueba es menor o igual al elemento a insertar se termina el while
+	addi 	$s0, $s4, 1		# $s0 = indice que es mayor por una unidad al indice de prueba
+	sll	$s0, $s0, 2		# Byte offset
+	sw	$t2, arreglo($s0)	# Se guarda el elemento de prueba en la posicion + 1
+	subi	$s4, $s4, 1		# Resto 1 al indice de prueba
+	j	lazo_while		# Regreso al inicio del while
+fin_del_wh:
+	addi 	$s0, $s4, 1		# $s0 = indice que es mayor por una unidad al indice de prueba
+	sll	$s0, $s0, 2		# Byte offset
+	sw	$t1, arreglo($s0)	# Se guarda el elemento a insertar en la posicion de prueba + 1
+	addi	$s1, $s1, 1		# Indice de elemento sin ordenar + 1
+	j	lazo_for		# Regresar al inicio del for
 imp:	
 	addi $s0, $s0, 4 #avanzo una posicion en el arreglo
 	lw $t8, arreglo($s0) #leo el numero
@@ -116,7 +152,7 @@ imp:
 	
 #DAR FORMATO AL ARCHIVO INSERT SORT:
 #Para darle formato al archivo, hay que agregar enters y espacios tras cada n?mero.
-	 	 addi   $s2, $s2, 2	# se le suman 2 a $s2 porque el indice comienza en en 1
+	 	 addi   $s2, $s2, 2	# se le suma 1 a $s2 porque el indice comienza en en 1
 		 addi	$s1, $zero, 1	# $s1 indice del arreglo
 		 j	loop_sin_coma	#El primer n?mero no necesita una enter que lo preceda as? que se omite este paso
 loop_con_coma:	 li	$v0, 15       	# Servicio 15 escribe archivos
